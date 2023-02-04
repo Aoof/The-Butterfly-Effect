c56842673266f55e9a9e3e8a2821979bc963a65c
Abdulrahman M <53632818+Aoof@users.noreply.github.com>
2023-02-03 21:24:49 -0500

    Functional Movement and animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 6f;
     private float playerVelocity;
-   
+    private Animator playerAnimator;
+
+    private void Start()
+    {
+        playerAnimator = gameObject.GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
-    {
+    {    
+
         playerVelocity = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(moveSpeed * playerVelocity, 0);
+
+        if (playerVelocity != 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = playerVelocity < 0;
+        }
+        playerAnimator.SetBool("Moving", playerVelocity != 0);
+
         transform.Translate(movement * Time.deltaTime);
     }
 }
