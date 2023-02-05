2b67394756579cd4f92e217dd8ea6632fef6a91c
Aoof <53632818+Aoof@users.noreply.github.com>
2023-02-04 20:33:26 -0500

    More movement and animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 6f;
+    public float boundary = 20f;
+    public float yShift = -5.25f;
+    public Transform[] playerBoundaries = { null, null };
     private float playerVelocity;
     private Animator playerAnimator;
 
@@ -21,11 +24,16 @@ public class PlayerController : MonoBehaviour
         Vector2 movement = new Vector2(moveSpeed * playerVelocity, 0);
 
         if (playerVelocity != 0)
-        {
             GetComponent<SpriteRenderer>().flipX = playerVelocity < 0;
-        }
+
         playerAnimator.SetBool("Moving", playerVelocity != 0);
 
         transform.Translate(movement * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, boundary *-1, boundary), yShift, 0);
+
+        if (playerBoundaries[0])
+            playerBoundaries[0].position = new Vector3(boundary * -1, yShift);
+        if (playerBoundaries[1])
+            playerBoundaries[1].position = new Vector3(boundary, yShift);
     }
 }
