4a0b853821044aae5c3438fdceefb819d2a1c49a
Cravyk <eveline54637@gmail.com>
2023-02-05 09:22:13 -0500

    SpawnItems

--- Assets/Scripts/Inventory system/Item.cs
+++ Assets/Scripts/Inventory system/Item.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Item", menuName = "Item/Create New Item")]
+[CreateAssetMenu(menuName = "Inventory Item Data")]
 public class Item : ScriptableObject
 {
     public int id;
---
+++ Assets/Scripts/Spawn Items/ItemAreaSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemAreaSpawner : MonoBehaviour
+{
+    public GameObject itemToSpread;
+    public int numItemsToSpawn = 10;
+
+    public float itemXSpread = 10;
+    public float itemYSpread = 0;
+    public float itemZSpread = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i =0; i < numItemsToSpawn; i++)
+        {
+            SpreadItem();
+        }
+    }
+
+    void SpreadItem()
+    {
+        Vector3 randPosition = new Vector3 (Random.Range(-itemXSpread, itemXSpread), Random.Range(-itemYSpread, itemYSpread), Random.Range(-itemZSpread, itemZSpread)) + transform.position;
+        GameObject clone = Instantiate(itemToSpread, randPosition, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
